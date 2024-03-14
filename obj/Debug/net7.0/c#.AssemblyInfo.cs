3cb28d2ea7201fcb0888fe3f32e55914810500ff
Zuzia <zuzia.jarl@gmail.com>
2024-03-14 15:51:20 +0100

    some changes

--- obj/Debug/net7.0/c#.AssemblyInfo.cs
+++ obj/Debug/net7.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1812b3dcdbf2ad8531a041a3d1d278b05e179c20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79fdd073510e30fa0a8846619cf192e4d0250d63")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
