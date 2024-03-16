f6b1b280e506cfbe4f90bd3bdc8941d8388e00fe
uknowmee <michalpalucki@icloud.com>
2024-03-16 18:20:36 +0100

    adjust project structure

---
+++ Labs/Lab03/Lab03.Task01/Comparers.cs
@@ -0,0 +1,20 @@
+namespace task1
+{
+    public class CompareByName : IComparer<Tweet>
+    {
+        public int Compare(Tweet t1, Tweet t2)
+        {
+            return t1.UserName.CompareTo(t2.UserName);
+        }
+    }
+
+    public class CompareByDate : IComparer<Tweet>
+    {
+        public int Compare(Tweet t1, Tweet t2)
+        {
+            DateTime convertedDate1 = DateTime.Parse(t1.CreatedAt.Replace("at", ""));
+            DateTime convertedDate2 = DateTime.Parse(t2.CreatedAt.Replace("at", ""));
+            return convertedDate1.CompareTo(convertedDate2);
+        }
+    }
+}
---
+++ Labs/Lab03/Lab03.Task01/Tweet.cs
@@ -0,0 +1,71 @@
+namespace task1
+{
+    public class Tweet
+    {
+        public string? Text { get; set; }
+        public string? UserName { get; set; }
+        public string? LinkToTweet { get; set; }
+        public string? FirstLinkUrl { get; set; }
+        public string? CreatedAt { get; set; }
+        public string? TweetEmbedCode { get; set; }
+
+        public override string ToString()
+        {
+            return $"Author: {UserName}, Text: {Text}";
+            ;
+        }
+
+        public bool Includes(string myWord)
+        {
+            string[] words = System.Text.RegularExpressions.Regex.Split(Text, @"\W+");
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    string formattedWord = word.ToLower();
+
+                    if (myWord == formattedWord)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        // public int CompareTo(Tweet? other){
+        //     return this.Text.CompareTo(other?.Text);
+        // }
+    }
+
+
+    public class Tweets
+    {
+        public List<Tweet> data { get; set; }
+
+        public void SortByNick()
+        {
+            CompareByName comp = new CompareByName();
+            data.Sort(comp);
+        }
+
+        public void SortByDate()
+        {
+            CompareByDate comp = new CompareByDate();
+            data.Sort(comp);
+        }
+
+        public Tweet FindOldest()
+        {
+            this.SortByDate();
+            return this.data[0];
+        }
+
+        public Tweet FindNewest()
+        {
+            this.SortByDate();
+            return this.data[data.Count - 1];
+        }
+    }
+}
