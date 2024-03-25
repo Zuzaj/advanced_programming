af8221c97c67b3e5e0c4c802d384221a70e8f287
Uknowme <michalpalucki@icloud.com>
2024-03-25 01:04:24 +0100

    use is to check for null

--- Labs/Lab04/ConsoleApp1/FileReader.cs
+++ Labs/Lab04/ConsoleApp1/FileReader.cs
@@ -5,15 +5,13 @@ public class FileReader<T>
     public List<T> ToList(string path, Func<string[], T> generate)
     {
         var list = new List<T>();
-        using (var reader = new StreamReader(path))
+        using var reader = new StreamReader(path);
+
+        reader.ReadLine();
+        while (reader.ReadLine() is { } line)
         {
-            string line;
-            line = reader.ReadLine();
-            while ((line = reader.ReadLine()) != null)
-            {
-                var features = line.Split(',');
-                list.Add(generate(features));
-            }
+            var features = line.Split(',');
+            list.Add(generate(features));
         }
 
         return list;
